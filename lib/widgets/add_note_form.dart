389ea1556c0ded66ba088117dc9d20c1da098334
Muhammad Osama <mo1318@fayoum.edu.eg>
2023-12-06 03:02:11 +0200

    date time format

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'custom_button.dart';
@@ -48,11 +49,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
+                    var currentTime = DateTime.now();
+                    var formattedCurrentDate = DateFormat.yMd().format(currentTime).toString();
                     var noteModel = NoteModel(
                         title: title!,
                         subTitle: subTitle!,
-                        date: DateTime.now().year.toString(),
-                        color: Colors.blue.value);
+                        date: formattedCurrentDate,
+                        color: Colors.green.value);
                     BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
                   } else {
                     autoValidateMode = AutovalidateMode.always;
