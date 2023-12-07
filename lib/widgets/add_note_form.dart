12d7d1acccb0c635d18ce70d250baf2c33de4cd7
Muhammad Osama <mo1318@fayoum.edu.eg>
2023-12-07 17:16:03 +0200

    create colors list view

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'colors_list_view.dart';
 import 'custom_button.dart';
 import 'custom_text_field.dart';
 import 'package:flutter/material.dart';
@@ -42,25 +43,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: 'Content',
             maxLines: 5,
           ),
+          const ColorListView(),
           BlocBuilder<AddNotesCubit, AddNotesState>(
             builder: (context, state) {
               return CustomButton(
                 isLoading: state is AddNoteLoading ? true : false,
                 onTap: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    var currentTime = DateTime.now();
-                    var formattedCurrentDate = DateFormat.yMd().format(currentTime).toString();
-                    var noteModel = NoteModel(
-                        title: title!,
-                        subTitle: subTitle!,
-                        date: formattedCurrentDate,
-                        color: Colors.green.value);
-                    BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
-                  } else {
-                    autoValidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
+                  validateForm(context);
                 },
               );
             },
@@ -69,4 +58,23 @@ class _AddNoteFormState extends State<AddNoteForm> {
       ),
     );
   }
+
+  void validateForm(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      var currentTime = DateTime.now();
+      var formattedCurrentDate =
+          DateFormat.yMd().format(currentTime).toString();
+      var noteModel = NoteModel(
+          title: title!,
+          subTitle: subTitle!,
+          date: formattedCurrentDate,
+          color: Colors.green.value);
+      BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
+    } else {
+      autoValidateMode = AutovalidateMode.always;
+      setState(() {});
+    }
+  }
 }
+
---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: CircleAvatar(
+        backgroundColor: Colors.blue,
+        radius: 36,
+      ),
+    );
+  }
+}
+
+class ColorListView extends StatelessWidget {
+  const ColorListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const ColorItem();
+          }),
+    );
+  }
+}
